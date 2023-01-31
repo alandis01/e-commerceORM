dee7000b6fed2b482a349557669a2b7aba43e9d6
ashley <alandis01@gmail.com>
2023-01-31 17:55:27 -0500

    added post route to category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,21 +17,32 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('./:id', async (req,res) => {
+router.get('./:id', async (req, res) => {
     try {
         const category = await Category.findByPk(req.params.id, {
             invlude: [{
-                model: Product, }]
+                model: Product,
+            }]
         });
 
         if (!category) {
             res.status(404).json({ message: "No category with this id can be found" });
         }
 
-        res.json(category);
+        res.status(200).json(category);
     } catch (err) {
-        res.json(err);
+        res.status(500).json(err);
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const category = await Category.create(req.body) 
+            res.status(200).json(category);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+
 
 
